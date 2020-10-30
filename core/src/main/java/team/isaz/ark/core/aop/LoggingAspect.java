8e5178e0ed5c1e109b11e44da07ccd16119a9d76
Arkanium77 <arkanium77@gmail.com>
2020-10-30 13:13:43 +0300

    logback fix
    - Исправлен logback.xml
    - В логгирующий аспект добавлено логгирование void-методов

--- core/src/main/java/team/isaz/ark/core/aop/LoggingAspect.java
+++ core/src/main/java/team/isaz/ark/core/aop/LoggingAspect.java
@@ -1,12 +1,14 @@
 package team.isaz.ark.core.aop;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.CodeSignature;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -16,37 +18,60 @@ import java.util.List;
 @Aspect
 @Component
 public class LoggingAspect {
+    private final int MAX_LENGTH = 90;
 
-    @Pointcut("within(team.isaz.ark.core.service.*)")
+    @Pointcut("within(team.isaz.ark.core.service..*)")
     public void debugLog() {
     }
 
-    @SneakyThrows
-    @Around("debugLog()")
-    public Object logIt(ProceedingJoinPoint jp) {
-        log.trace("\u001B[35m===== LOGGING ASPECT IN=====\u001B[0m");
+    @AfterThrowing(value = "debugLog()", throwing = "t")
+    public void afterThrow(JoinPoint jp, Throwable t) {
         CodeSignature methodSignature = (CodeSignature) jp.getSignature();
-        StringBuilder builder = new StringBuilder("Method ").append(methodSignature.getName())
-                .append(" run with args:");
+        log.error("{} throws unexpected {} : {}", methodSignature.getName(), t.getClass().getSimpleName(), t.getMessage());
+    }
 
-        Object[] args = jp.getArgs();
-        List<String> sigParamNames = Arrays.asList(methodSignature.getParameterNames());
-        for (int i = 0; i < sigParamNames.size(); i++) {
-            builder.append("\n\t").append(sigParamNames.get(i))
-                    .append(" = ")
-                    .append(args[i]);
-        }
-        log.debug("{}", builder.toString());
-        try {
-            Object o = jp.proceed(args);
-            log.debug("Method {} successful return \n {}", methodSignature.getName(), o);
-            log.trace("\u001B[35m===== ASPECT OUT =====\u001B[0m");
-            return o;
-        } catch (Throwable throwable) {
-            log.error("Unexpected {} : {}", throwable.getClass().getSimpleName(), throwable.getMessage());
-            log.trace("\u001B[35m===== ASPECT OUT =====\u001B[0m");
-            throw throwable;
+    @AfterReturning(value = "debugLog()", returning = "result")
+    public void afterReturn(JoinPoint jp, Object result) {
+        CodeSignature methodSignature = (CodeSignature) jp.getSignature();
+        if (((MethodSignature) jp.getSignature()).getReturnType().toString().equals("void")) {
+            log.debug("The method {} has correctly completed the work.", methodSignature.getName());
+        } else {
+            if (result != null) {
+                if (result.toString().length() < MAX_LENGTH) {
+                    log.debug("The method {} has correctly completed the work and return:\n\t- {}", methodSignature.getName(), result);
+                } else {
+                    log.debug("The method {} has correctly completed the work and return {}", methodSignature.getName(), result.getClass().getSimpleName());
+
+                }
+
+            } else {
+                log.debug("The method {} has correctly completed the work and return NULL value", methodSignature.getName());
+            }
         }
     }
 
+    @Before("debugLog()")
+    public void before(JoinPoint jp) {
+        CodeSignature methodSignature = (CodeSignature) jp.getSignature();
+        StringBuilder builder;
+        if (jp.getArgs().length == 0) {
+            builder = new StringBuilder("Method ").append(methodSignature.getName())
+                    .append(" run with no args");
+        } else {
+            builder = new StringBuilder("Method ").append(methodSignature.getName())
+                    .append(" run with args:");
+
+            Object[] args = jp.getArgs();
+            List<String> sigParamNames = Arrays.asList(methodSignature.getParameterNames());
+            for (int i = 0; i < sigParamNames.size(); i++) {
+                builder.append("\n\t").append(sigParamNames.get(i))
+                        .append(" = ")
+                        .append(args[i] != null
+                                && args[i].toString().length() > MAX_LENGTH ?
+                                args[i].getClass().getSimpleName() + " {...}"
+                                : args[i]);
+            }
+        }
+        log.debug("{}", builder.toString());
+    }
 }
